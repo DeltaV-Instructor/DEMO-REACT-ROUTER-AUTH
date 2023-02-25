e33013e7336625847d335ae4c2322a88f986c2a2
tektechnologies <info@tektechnologies.com>
2023-02-25 13:19:56 -0600

    set up app for router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-
 import React from "react";
+import AboutUs from "./pages/AboutUs";
+import FAQ from "./pages/FAQ";
+import Home from "./pages/Home";
+import NavLayout from "./components/NavLayout";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<NavLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<AboutUs />} />
+      <Route path="faq" element={<FAQ />} />
+    </Route>
+  )
+);
 
 class App extends React.Component {
-
-
-
-
-
-
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+      <>
+        <RouterProvider router={router} />
+      </>
+    );
   }
 }
 
